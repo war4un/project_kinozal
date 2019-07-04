bddbc93bfb31405f5a89a18594dcde2251036bf3
Alexander Morozov <frostmoroz83@yandex.ru>
2019-07-04 20:02:58 +0400

    обновил singloutlinks

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import {Col, Nav, NavItem, Row} from 'reactstrap';
 
 const SignedOutLinks = () => {
     return(
-        <ul className="right">
-            <li><NavLink to='/signin'>Войти</NavLink></li>
-            <li><NavLink to='/signup'>Регистрация</NavLink></li>
-        </ul>
+        <Col xs="8 offset-4" >
+            <Nav navbar>
+                <Row className="justify-content-end flex-nowrap align-items-center">
+                    <Col >
+                        <NavItem><NavLink to='/signin' style={{textDecoration: 'none'}}>
+                            <div className="header_title">
+                                Войти
+                            </div>
+                        </NavLink></NavItem>
+                    </Col>
+                    <Col>
+                        <NavItem><NavLink to='/signup' style={{textDecoration: 'none'}}>
+                            <div className="header_title">
+                                Регистрация
+                            </div>
+                        </NavLink></NavItem>
+                    </Col>
+                </Row>
+            </Nav>
+        </Col>
     )
 }
 
