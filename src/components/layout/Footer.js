14fb91aa57c02d83a316df28ab14b4591e311165
Alexander Morozov <frostmoroz83@yandex.ru>
2019-07-04 19:45:36 +0400

    обновил navbar

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Col, Row, Container} from 'reactstrap'
+import './Footer.css'
+import facebook from '../../img/icons8-facebook-48.png'
+import vk from '../../img/icons8-vk-circled-48.png'
+
+import cinema_logo from '../../img/cinema-logo.png'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <Container>
+                <Row className="justify-content-between flex-nowrap align-items-center pt-2 pb-2">
+                    <Col xs="3" className="footer_img">
+                        <img src={cinema_logo} alt="RelaxCinema"/>
+                    </Col>
+                    <Col xs="5" className="soc">
+                        <div className="address">Кинотеатр RelaxCinema © 2019. Все права защищены.</div>
+                    </Col>
+                    <Col xs="4" className="address">
+                        <Row className="d-flex justify-content-between flex-column align-items-start">
+                            <div>Ульяновск</div>
+                            <div>проспект Нариманова, дом 1,</div>
+                            <div>строение 2, 3-й этаж (главный офис)</div>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,23 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
-import { connect } from 'react-redux'
-
-
+import {connect} from 'react-redux'
+import cinema_logo from '../../img/cinema-logo.png'
+import './NavBar.css'
+import {Container, Row, Col, CardImg} from 'reactstrap';
 
 const Navbar = (props) => {
-    const { auth } = props;
-    const links = auth.uid  ? <SignedInLinks /> : <SignedOutLinks />
-    return(
-        <nav className="nav-wrapper grey darken-3">
-            <div className="container">
-                <Link to='/' className="brand-logo">title</Link>
-                { links }
-            </div>
-        </nav>
+    const {auth} = props;
+    const links = auth.uid ? <SignedInLinks/> : <SignedOutLinks/>
+    return (
+        <div className="nav_bar">
+            <Container>
+                <Row className="justify-content-between flex-nowrap align-items-center pt-2 pb-2">
+                    <Col  xs="5" className="footer_img">
+                        <Row className="justify-content-start flex-nowrap align-items-center">
+                            <Col xs="3">
+                                <Link to='/' style={{textDecoration: 'none'}}>
+                                    <img src={cinema_logo} alt="RelaxCinema"/>
+                                </Link>
+                            </Col>
+                            <Col>
+                                <Link to="/" style={{textDecoration: 'none'}}>
+                                    <div className="heder_title">
+                                        RelaxCinema
+                                    </div>
+                                </Link>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col xs="7">
+                        <Row className="justify-content-end flex-nowrap align-items-center">
+                            {links}
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
-}
+
+};
 
 const mapStateToProps = (state) => {
     return {
