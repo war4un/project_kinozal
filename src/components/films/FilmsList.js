4f11767d9fe7f82a8fff1c894fafb8bfd346f25d
Alexander Morozov <frostmoroz83@yandex.ru>
2019-07-04 19:31:48 +0400

    up 2

--- src/components/films/FilmsList.js
+++ src/components/films/FilmsList.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import FilmsWrapper from './FilmsWrapper'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import {Col} from 'reactstrap';
+import './FilmsWrapper.css'
 
 
 const FilmsList = ({films}) => {
     return(
-        <div className="section">
-           {films && films.map(film =>{
-               return(
-                <Link to={'/films/' + film.id} key={film.id}>
-                        <FilmsWrapper film={film} key={film.id} />
+        <Col xs="4" className="content_card">
+            {films && films.map(film => {
+                return (
+                    <Link to={'/films/' + film.id} key={film.id}>
+                        <FilmsWrapper film={film} key={film.id}/>
                     </Link>
-               )
-           })}
-        </div>
+                )
+            })}
+        </Col>
     )
 }
 
--- src/components/films/FilmsWrapper.js
+++ src/components/films/FilmsWrapper.js
@@ -1,29 +1,28 @@
 import React from 'react'
-
+import {Row, Col} from 'reactstrap';
 
 
 const FilmsWrapper = ({film}) => {
     return(
-        <div className="col m4 l3 card-content grey-text text-darken-3">
-        <div className="card large sticky-action hoverable">
-            <div className="card-image waves-effect waves-block waves-light">
-                <img className="responsive-img activator" src={film.img} alt={film.title}/>
-                <span className="card-title" style={{}}>{film.age}</span>
-            </div>
-            <div className="card-content">
-                <span className="card-title grey-text text-darken-4"><h6>{film.title}</h6></span>
-            </div>
-            <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4"><i
-                    className="material-icons right">close</i></span>
-                <p>{film.content}</p>
-            </div>
-            <div className="card-action">
-                <div>Купить билет на фильм</div>
-            </div>
-
-        </div>
-    </div>
+        <Row className="d-flex">
+            <Col xs="4" className="content_card-img">
+                <img className="content_card-img__item" src={film.img} alt={film.title}/>
+            </Col>
+            <Col xs="8">
+                <Row>
+                    <div className="card-content">
+                        <span className="">
+                            <h6>{film.title}</h6>
+                        </span>
+                    </div>
+                    <div className="card-reveal">
+                        <span className="">
+                            <i className="">close</i>
+                        </span>
+                    </div>
+                </Row>
+            </Col>
+        </Row>
     )
 }
 
