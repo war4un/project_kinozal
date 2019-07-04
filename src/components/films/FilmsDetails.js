51cb53c89f227ccdd91937d62e4d5bc1705cfec4
Alexander Morozov <frostmoroz83@yandex.ru>
2019-07-05 00:15:25 +0400

    css деталей

--- src/components/films/FilmsDetails.js
+++ src/components/films/FilmsDetails.js
@@ -1,79 +1,67 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { firestoreConnect } from 'react-redux-firebase'
-import { compose } from 'redux'
-import { NavLink } from 'react-router-dom'
-import { Redirect } from 'react-router-dom'
+import {connect} from 'react-redux'
+import {firestoreConnect} from 'react-redux-firebase'
+import {compose} from 'redux'
+import {NavLink} from 'react-router-dom'
+import {Redirect} from 'react-router-dom'
+import {Row, Col} from 'reactstrap';
+import {Card, CardBody, Button, CardTitle, CardText, CardImg} from 'reactstrap';
 
 
 const FilmsDetails = (props) => {
-  const { film, auth } = props;
-  if (film) {
-    return (
-        <div className="container section">
-        <div className="row">
-            <div className="col s3">
-                <div className="card">
-                    <div className="card-image">
-                        <img className="responsive-img " src={film.img} alt={film.title}/>
-                        <span className="card-title" style={{}}>{film.age}</span>
+    const {film, auth} = props;
+    if (film) {
+        return (
+            <Row className="card_detalis flex-column justify-content-around align-items-center">
+                <Col className="float-left">
+                    <div className="d-title">
+                        <h1>{film.title}</h1>
                     </div>
-                    <div className="card-content">
-                        <span className="card-title grey-text text-darken-4"><h6>{film.title}</h6></span>
-                    </div>
-                </div>
+                </Col>
+                <Col>
+                    <Row className="justify-content-between">
+                        <Col xs="4" className="d-img">
+                            <Card claccName="card_item">
+                                <CardImg className="card_img" top width="100%" src={film.img} alt={film.title}/>
+                            </Card>
+                        </Col>
+                        <Col xs="8" className="d-content"></Col>
+                    </Row>
+                </Col>
+            </Row>
+
+        )
+    } else {
+        return (
+            <div className="container center">
+                <p>Loading films...</p>
             </div>
-            <div className="col s9 card-content grey-text text-darken-3">
-                <h3>{film.title}</h3>
-                <div style={{border: "1px solid red", padding: "20px"}}>{film.content}</div>
-                <h4>Выбор сеанса</h4>
+        )
+    }
+}
 
-                    <div className="row">
-                        <div className="col s2">
-                          <p className="waves-effect waves-light btn">
-                            <li><NavLink to='/hallo'>1400</NavLink></li>
-                          </p>
-                        </div>
-                        <div className="col s2">
-                          <p className="waves-effect waves-light btn">
-                            <li><NavLink to='/hallo3d'>1400</NavLink></li>
-                          </p>
-                        </div>
-                        <div className="col s2">
-                          <p className="waves-effect waves-light btn">
-                            <li><NavLink to='/hallorelax'>1400</NavLink></li>
-                          </p>
-                        </div>
-                    </div>
-                <div className="video-container">
-                  <iframe title={film.title} src={film.video} width="640" height="360" frameborder="0" allowfullscreen ></iframe>
-                </div>
-                </div>
-        </div>
-    </div>
+{/*<div className="video-container">*/
+}
+{/*    <iframe title={film.title} src={film.video} width="640" height="360" frameBorder="0" allowFullScreen></iframe>*/
+}
+{/*</div>*/
+}
 
-    )
-  } else {
-    return (
-      <div className="container center">
-        <p>Loading films...</p>
-      </div>
-    )
-  }
+{/*<li><NavLink to='/hallo'>1400</NavLink></li>*/
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const id = ownProps.match.params.id;
-  const films = state.firestore.data.film;
-  const film = films ? films[id] : null
-  return {
-    film: film
-  }
+    const id = ownProps.match.params.id;
+    const films = state.firestore.data.film;
+    const film = films ? films[id] : null
+    return {
+        film: film
+    }
 }
 
 export default compose(
-  connect(mapStateToProps),
-  firestoreConnect([{
-    collection: 'film'
-  }])
+    connect(mapStateToProps),
+    firestoreConnect([{
+        collection: 'film'
+    }])
 )(FilmsDetails)
